0a5ecdb6904a4eff211ee746601fefab74132cb1
Osman Zakir <osmanzakir90@hotmail.com>
2019-10-02 16:23:00 +0500

    adding initial files

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function createList() {
+    const unorderedList = document.createElement("ul");
+    let listItemContent;
+    while (listItemContent = prompt("Enter a list item; press \"Cancel\" to stop")) {
+        const listItem = document.createElement("li");
+        listItem.textContent = listItemContent;
+        unorderedList.append(listItem);
+    }
+    document.body.append(unorderedList);
+}
+
+createList();
